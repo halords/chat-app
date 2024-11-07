fce863bfa67935bc2af0007e08e3b64a117eeda4
halords <haroldjamora08@gmail.com>
2024-11-07 15:41:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ function broadcast(data) {
   });
 }
 
-server.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
